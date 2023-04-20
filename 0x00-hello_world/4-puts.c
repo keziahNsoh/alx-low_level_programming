304dc52fa385d7908c682f08a5fa6f0c88367b23
keziahNsoh <porepeya@gmail.com>
2023-04-20 01:54:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+* main - prints "Programming is like building a multilingual puzzle, followed by a new line.
 * Return: Always 0.
- */
+*/
 
 int main(void)
 
