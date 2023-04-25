4b3165f6cbaeb24e47897a5ab6be8cdcd384e824
keziahNsoh <porepeya@gmail.com>
2023-04-25 13:16:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+#include <unistd.h>
+
+
+/**
+ * print_alphabet - Entry point
+ * Return: void
+ */
+
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
