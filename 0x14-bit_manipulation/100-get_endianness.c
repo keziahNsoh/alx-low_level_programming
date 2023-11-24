fffa1869779d604503a9226f88a1e55269ad9285
keziahNsoh <porepeya@gmail.com>
2023-11-23 17:15:21 -0800

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 
 /**
  * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
+ * Return: 0 if big endian, 1 if little endian
  */
 
 int get_endianness(void)
