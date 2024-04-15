669ba8f745a0078039018324fee324556aeb9ae0
keziahNsoh <porepeya@gmail.com>
2024-04-15 16:40:03 -0700

    correction

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,36 +1,21 @@
-unsigned long int hash_djb2(const unsigned char *str)
-{
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
-}
-julien@ubuntu:~/0x1A. Hash tables$ 
-julien@ubuntu:~/0x1A. Hash tables$ cat 1-main.c 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_djb2 - function to compute hash of 'str' using djb2
+ * @str: string to hash
+ * Return: value of 64 bits hash
  */
-int main(void)
-{
-    char *s;
 
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    return (EXIT_SUCCESS);
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+	
+	hash = 5381;
+	
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
