af7457892a28db62f775ac4ebc0bd46e72e764ef
keziahNsoh <porepeya@gmail.com>
2024-04-15 19:07:48 -0700

    solution 6

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,21 +1,22 @@
 #include "hash_tables.h"
+
 /**
  * sorted_list - function for insert new node in sorted
  * @ht: pointer to sorted hash table
  * @new_node: pew node to insert
- *
  */
 void sorted_list(shash_table_t *ht, shash_node_t *new_node)
 {
 	shash_node_t *sbucket = ht->shead;
-
+	
 	if (sbucket == NULL)
 	{
 		ht->shead = ht->stail = new_node;
 		new_node->snext = new_node->sprev = NULL;
 		return;
 	}
-	do {
+	do
+	{
 		if (strcmp(new_node->key, sbucket->key) < 0)
 		{
 			new_node->snext = sbucket;
@@ -29,7 +30,8 @@ void sorted_list(shash_table_t *ht, shash_node_t *new_node)
 			return;
 		}
 		sbucket = sbucket->snext;
-	} while (sbucket);
+	}
+	while (sbucket);
 	new_node->sprev = ht->stail;
 	new_node->snext = ht->stail->snext;
 	ht->stail->snext = new_node;
@@ -39,20 +41,17 @@ void sorted_list(shash_table_t *ht, shash_node_t *new_node)
 /**
  * shash_table_create - creates a sorted hash tables
  * @size: size of the hash table
- *
  * Return: pointer to hash table
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
 	shash_table_t *table;
-
 	if (size == 0)
 		return (NULL);
-
+	
 	table = calloc(1, sizeof(shash_table_t));
 	if (table == NULL)
 		return (NULL);
-
 	table->size = size;
 	table->array = calloc(size, sizeof(shash_node_t *));
 	if (table->array == NULL)
@@ -68,7 +67,6 @@ shash_table_t *shash_table_create(unsigned long int size)
  * @ht: pointer to sorted hash table
  * @key: key to add the element
  * @value: value to add the element
- *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
@@ -76,7 +74,6 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	char *valuecopy, *keycopy;
 	shash_node_t  *bucket, *new_node;
-
 	if (!ht || !key || !*key || !value)
 		return (0);
 	valuecopy = strdup(value);
@@ -84,10 +81,9 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	bucket = ht->array[index];
-
 	while (bucket)
 	{
-if (!strcmp(key, bucket->key))
+		if (!strcmp(key, bucket->key))
 		{
 			free(bucket->value);
 			bucket->value = valuecopy;
@@ -115,14 +111,12 @@ if (!strcmp(key, bucket->key))
  * shash_table_get - function to retrieve value associeted with key
  * @ht: pointer to hash table
  * @key: key to retrive value
- *
  * Return: value or NULL if key does not exit.
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 	shash_node_t  *bucket;
-
 	if (!ht || !key || !*key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
@@ -139,13 +133,11 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 /**
  * shash_table_print - function to print the key:value from ht
  * @ht: pointer to hash table
- *
  */
 void shash_table_print(const shash_table_t *ht)
 {
 	shash_node_t *bucket;
 	int not_fin = 0;
-
 	if (!ht)
 		return;
 	bucket = ht->shead;
@@ -163,19 +155,17 @@ void shash_table_print(const shash_table_t *ht)
 /**
  * shash_table_print_rev - function to print the key:value from ht in reverse
  * @ht: pointer to hash table
- *
  */
 void shash_table_print_rev(const shash_table_t *ht)
 {
 	shash_node_t *bucket;
 	int not_fin = 0;
-
 	if (!ht)
 		return;
 	bucket = ht->stail;
 	printf("{");
 	while (bucket)
-{
+	{
 		if (not_fin)
 			printf(", ");
 		printf("'%s': '%s'", bucket->key, bucket->value);
@@ -187,16 +177,13 @@ void shash_table_print_rev(const shash_table_t *ht)
 /**
  * shash_table_delete - free hash table and all nodes
  * @ht: pointer to hash table
- *
  */
 void shash_table_delete(shash_table_t *ht)
 {
 	shash_node_t *bucket, *aux_free;
 	unsigned long int i = 0;
-
 	if (!ht)
 		return;
-
 	for (i = 0; i < ht->size; i++)
 	{
 		bucket = ht->array[i];
